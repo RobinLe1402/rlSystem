84b130e3bbbb65ac5e9f399ba625832785e01496
RobinLe <web@robinle.de>
2023-09-28 15:09:30 +0200

    code documentation

--- include/rlSystem/WindowsUnicodeString.hpp
+++ include/rlSystem/WindowsUnicodeString.hpp
@@ -21,8 +21,10 @@ namespace rlSystem
 		using OSChar   = wchar_t;
 		using OSString = std::wstring;
 
+		/// <summary>Convert a UTF-8 string to a Windows UTF-16 string.</summary>
 		OSString ToOS(const char8_t *szUTF8) noexcept;
 
+		/// <summary>Convert a Windows UTF-16 string to a UTF-8 string.</summary>
 		std::u8string FromOS(const OSChar *szOS) noexcept;
 
 	}
